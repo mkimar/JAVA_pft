f836c8766cee3634b304d1e51fde86f526c360e7
mkimar <mkimar@mail.ru>
2016-02-22 16:25:22 +0600

    форматирование кода

--- sandbox/src/main/java/ru/stga/pft/sandbox/Mytest.java
+++ sandbox/src/main/java/ru/stga/pft/sandbox/Mytest.java
@@ -1,33 +1,34 @@
 package ru.stga.pft.sandbox;
 
-    public class Mytest {
+public class Mytest {
 
-	  public static void main(String[] args) {
-			hello("world");
-			hello("user");
-			hello("Marina");
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Marina");
 
-			double l = 5;
-			System.out.println("Площадь квадрата со стороной " +  l  + " равна " + area(l));
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " равна " + area(l));
 
-			double a = 4;
-			double b = 6;
-			System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a,b));
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " равна " + area(a, b));
 
-			}
+  }
 
 
-		public static void hello (String somebady) {
+  public static void hello(String somebady) {
 
-				System.out.println("Hello," + somebady + "!!!");
-			}
+    System.out.println("Hello," + somebady + "!!!");
+  }
 
-			public static double area (double len) {
-				return len * len;
-			}
-			public  static double area (double a, double b){
-				return a * b;
-			}
+  public static double area(double len) {
+    return len * len;
+  }
 
-      }
+  public static double area(double a, double b) {
+    return a * b;
+  }
+
+}
 
