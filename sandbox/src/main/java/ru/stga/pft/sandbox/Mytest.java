22e13aa73b9a5adcba11a2976929e7cfa7184fb9
mkimar <mkimar@mail.ru>
2016-02-22 14:27:15 +0600

    Добавлен пакет

--- sandbox/src/main/java/ru/stga/pft/sandbox/Mytest.java
+++ sandbox/src/main/java/ru/stga/pft/sandbox/Mytest.java
@@ -1,7 +1,10 @@
-public class Mytest {
+package ru.stga.pft.sandbox;
+
+    public class Mytest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello,world!");
+
       }
 
 	}
