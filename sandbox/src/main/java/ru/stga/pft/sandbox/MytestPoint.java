4b8404fb35ee53f822da8b4095c755d38dc802f9
mkimar <mkimar@mail.ru>
2016-02-23 16:51:04 +0600

    Изменено название функции

--- sandbox/src/main/java/ru/stga/pft/sandbox/MytestPoint.java
+++ sandbox/src/main/java/ru/stga/pft/sandbox/MytestPoint.java
@@ -1,5 +1,7 @@
 package ru.stga.pft.sandbox;
 
+import static java.lang.Math.sqrt;
+
 /**
  * Created by marina on 23.02.2016.
  */
@@ -16,7 +18,7 @@ public class MytestPoint {
     double y2 = 6;
 
     System.out.println("Расстояние между двумя точками А с координатами (" + x1 + " ; " + y1 + ")" +
-            " и " + "B с координатами (" + x2 + ";" + y2 + ")" + " равна " + squareRoot(x1, x2, y1, y2));
+            " и " + "B с координатами (" + x2 + ";" + y2 + ")" + " равна " + distance(x1, x2, y1, y2));
 
   }
 
@@ -25,10 +27,9 @@ public class MytestPoint {
     System.out.println("Hello," + somebady + "!!!");
   }
 
+    public static double distance(double x1, double x2, double y1, double y2) {
 
-  public static double squareRoot(double x1, double x2, double y1, double y2) {
-
-    return (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1);
+    return sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 
 
   }
