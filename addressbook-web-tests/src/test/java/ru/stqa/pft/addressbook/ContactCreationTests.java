4e04a55af8fcf69a868a06e1780103d1595bfc86
mkimar <mkimar@mail.ru>
2016-02-29 01:05:21 +0600

    Параметризован метод заполнения формы Контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -33,7 +33,7 @@ public class ContactCreationTests {
   @Test
   public void testContactCreation() {
     newContactCreationPage();
-    fillContactForm();
+    fillContactForm("Contact222", "CCC222", "ContactTest", "con222", "c011", "c022", "c033", "c044", "bbbb", "c055", "dd", "mkimar@mail.ru", "c066");
     submitContactForm();
     logout();
 
@@ -47,46 +47,46 @@ public class ContactCreationTests {
     wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
   }
 
-  private void fillContactForm() {
+  private void fillContactForm(String fname, String mname, String lname, String nickname, String title, String company, String adress1, String domtel, String mobiltel, String work, String fax, String email2, String adress2) {
     wd.findElement(By.name("firstname")).click();
     wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("Contact222");
+    wd.findElement(By.name("firstname")).sendKeys(fname);
     wd.findElement(By.name("middlename")).click();
     wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys("CCC222");
+    wd.findElement(By.name("middlename")).sendKeys(mname);
     wd.findElement(By.name("lastname")).click();
     wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("ContactTest");
+    wd.findElement(By.name("lastname")).sendKeys(lname);
     wd.findElement(By.name("nickname")).click();
     wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys("con222");
+    wd.findElement(By.name("nickname")).sendKeys(nickname);
     wd.findElement(By.name("title")).click();
     wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys("c011");
+    wd.findElement(By.name("title")).sendKeys(title);
     wd.findElement(By.name("company")).click();
     wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys("c022");
+    wd.findElement(By.name("company")).sendKeys(company);
     wd.findElement(By.name("address")).click();
     wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("c033");
+    wd.findElement(By.name("address")).sendKeys(adress1);
     wd.findElement(By.name("home")).click();
     wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys("c044");
+    wd.findElement(By.name("home")).sendKeys(domtel);
     wd.findElement(By.name("mobile")).click();
     wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("123456");
+    wd.findElement(By.name("mobile")).sendKeys(mobiltel);
     wd.findElement(By.name("work")).click();
     wd.findElement(By.name("work")).clear();
-    wd.findElement(By.name("work")).sendKeys("c055");
+    wd.findElement(By.name("work")).sendKeys(work);
     wd.findElement(By.name("fax")).click();
     wd.findElement(By.name("fax")).clear();
-    wd.findElement(By.name("fax")).sendKeys("654321");
+    wd.findElement(By.name("fax")).sendKeys(fax);
     wd.findElement(By.name("email2")).click();
     wd.findElement(By.name("email2")).clear();
-    wd.findElement(By.name("email2")).sendKeys("mkimar@mail.ru");
+    wd.findElement(By.name("email2")).sendKeys(email2);
     wd.findElement(By.name("address2")).click();
     wd.findElement(By.name("address2")).clear();
-    wd.findElement(By.name("address2")).sendKeys("c066");
+    wd.findElement(By.name("address2")).sendKeys(adress2);
   }
 
   private void newContactCreationPage() {
