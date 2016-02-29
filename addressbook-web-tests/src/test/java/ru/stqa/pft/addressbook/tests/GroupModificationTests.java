ac50d0edba2387a4ba29b66bf7c9838aa03ec77c
mkimar <mkimar@mail.ru>
2016-02-29 23:31:40 +0600

    Создан тест-класс по изменению сведений в группе

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.addressbook.tests;
+
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+
+  public void testGroupModification () {
+
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupsHelper().selectGroup();
+    app.getGroupsHelper().initGroupModification();
+    app.getGroupsHelper().fillGroupForm(new GroupData("TestEDIT", "as_02", "as_03"));
+    app.getGroupsHelper().submitGroupModification();
+    app.getGroupsHelper().returnToGroupPage();
+  }
+
+}
