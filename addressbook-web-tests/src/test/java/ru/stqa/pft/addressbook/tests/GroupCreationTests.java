a653926b568812c92c34eff11702ebe6f2413837
mkimar <mkimar@mail.ru>
2016-03-18 16:12:43 +0600

    В тест по созданию группы добавлено сравнение множеств с помощью лямбда-выражения

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -20,26 +20,7 @@ public class GroupCreationTests extends TestBase {
     List<GroupData> after = app.getGroupsHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (GroupData g : after) {
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-
-    Comparator<? super GroupData> byId = new Comparator<GroupData>() {
-
-    @Override
-      public int compare(GroupData o1, GroupData o2) {
-        return Integer.compare(o1.getId(), o2.getId());
-      }
-    };
-
-    int max1 = after.stream().max(byId).get().getId();
-
-
-
-    group.setId(after.stream().max(o1, o2) ;
+    group.setId(after.stream().max((o1, o2)-> Integer.compare(o1.getId(),o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
 
