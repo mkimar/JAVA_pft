c0a594c8d089d364f0688be8c243695241e1ebfa
mkimar <mkimar@mail.ru>
2016-03-21 13:12:06 +0600

    Создан тест для Просмотра Анализа свойств Профиля класса

--- profileClassa-web-tests/src/main/java/kz/stqa/pft/newproect/AnalyzeTest.java
+++ profileClassa-web-tests/src/main/java/kz/stqa/pft/newproect/AnalyzeTest.java
@@ -6,29 +6,18 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-public class ClassAnalyzeTests {
-  FirefoxDriver wd;
+public class AnalyzeTest {
 
-  @BeforeMethod
 
-  public void main() throws Exception {
-    wd = new FirefoxDriver();
+  public static void main(String[] args) throws Exception {
+    FirefoxDriver wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     wd.get("http://in-capsule.com:9090/Workflow/login");
-    login();
-
-  }
-
-  public void login() {
     wd.findElement(By.id("login")).click();
     wd.findElement(By.id("login")).clear();
     wd.findElement(By.id("login")).sendKeys("user2");
@@ -36,11 +25,6 @@ public class ClassAnalyzeTests {
     wd.findElement(By.id("pwd")).clear();
     wd.findElement(By.id("pwd")).sendKeys("1");
     wd.findElement(By.id("enter")).click();
-  }
-
-  @Test
-  public void testClassAnalyze() throws IOException {
-
     wd.findElement(By.xpath("//li/div")).click(); // Открыть плюсик в меню Управление бизнес-классами
     wd.findElement(By.xpath("//a[@class='prmenu']/b[text()='Анализ свойств классов']")).click();// Кликнуть по Анализ свойств классов
     wd.findElement(By.xpath("//div[@id='tCLASSATTRTREE']/ul/li/span/span")).click(); // Открыть дерево классов щелкнув по плюсику
@@ -61,17 +45,19 @@ public class ClassAnalyzeTests {
 
   }
 
-  public void screenPage() throws IOException {
+  public static void screenPage() throws IOException {
+    FirefoxDriver wd = new FirefoxDriver();
     File screenshot = ((TakesScreenshot) wd).getScreenshotAs(OutputType.FILE);
     String path = "c:/Users/marina/Downloads/" + screenshot.getName();
     FileUtils.copyFile(screenshot, new File(path));
   }
 
 
-  @AfterMethod
-  public void tearDown() {
+  public static void tearDown() {
+    FirefoxDriver wd = new FirefoxDriver();
     wd.quit();
   }
+
 
   public static boolean isAlertPresent(FirefoxDriver wd) {
     try {
