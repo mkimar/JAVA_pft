407a82583676a409470268c208eb61024293304b
mkimar <mkimar@mail.ru>
2016-03-22 11:36:47 +0600

    Внесены изменения в тест по созданию класса

--- profileClassa-web-tests/src/test/java/kz/stqa/pft/newproect/ClassCreationTests.java
+++ profileClassa-web-tests/src/test/java/kz/stqa/pft/newproect/ClassCreationTests.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -38,112 +37,106 @@ public class ClassCreationTests {
 
   @Test
   public void testClassCreation() {
-    wd.findElement(By.xpath("//ul[@id='navigation']/li[1]/div")).click();
-    wd.findElement(By.xpath("//b[text()=\"Создать бизнес-класс\"]")).click();
-    wd.findElement(By.linkText("создать")).click();
-    wd.switchTo().alert().accept();
-    //Выбор НПА
-    wd.findElement(By.id("ufd-npa_select")).click();
-    wd.findElement(By.xpath("//div[@id='ufd-container']/div[2]//li[2]")).click();
-    if (!wd.findElement(By.xpath("//select[@id='npa_select']//option[2]")).isSelected()) {
-    wd.findElement(By.xpath("//select[@id='npa_select']//option[2]")).click();
-    }
-    wd.findElement(By.id("close_npa")).click();
-    //Выбор уровня секретности
-    wd.findElement(By.id("ufd-fSECURITY_ID")).click();
+    wd.findElement(By.xpath("//ul[@id='navigation']/li[1]/div")).click();//Раскрыть плюс Управление бизнес-классами
+    wd.findElement(By.xpath("//b[text()=\"Создать бизнес-класс\"]")).click();//Раскрыть Создать бизнес-класс
+    wd.findElement(By.linkText("создать")).click();// Создать новый Профиль класса
+    wd.switchTo().alert().accept();// в информац.сообщении щелкнуть ОК
+
+    wd.findElement(By.id("ufd-npa_select")).click();  //Выбор НПА
+    wd.findElement(By.xpath("//div[@id='ufd-container']/div[2]//li[2]")).click(); //Выбор НПА
+    if (!wd.findElement(By.xpath("//select[@id='npa_select']//option[2]")).isSelected()) {  //Выбор НПА
+      wd.findElement(By.xpath("//select[@id='npa_select']//option[2]")).click();
+    }  //Выбор НПА
+    wd.findElement(By.id("close_npa")).click(); // Далее
+
+    wd.findElement(By.id("ufd-fSECURITY_ID")).click();//вкладка Описание. Выбор уровня секретности
     wd.findElement(By.name("7")).click();//"Открытая Информация"
-    if (!wd.findElement(By.xpath("//select[@id='fSECURITY_ID']//option[8]")).isSelected()) {
-      wd.findElement(By.xpath("//select[@id='fSECURITY_ID']//option[8]")).click();
-    }
+    if (!wd.findElement(By.xpath("//select[@id='fSECURITY_ID']//option[8]")).isSelected()) {//Выбор уровня секретности
+      wd.findElement(By.xpath("//select[@id='fSECURITY_ID']//option[8]")).click();//Выбор уровня секретности
+    }//Выбор уровня секретности
     wd.findElement(By.id("saveProfile")).click();//Сохранить
-    //new Select(wd.findElement(By.id("fSECURITY_ID"))).selectByVisibleText("Открытая Информация");
-    wd.findElement(By.id("fCLASS_NAME")).click();
-    wd.findElement(By.id("fCLASS_NAME")).clear();
-    wd.findElement(By.id("fCLASS_NAME")).sendKeys("Куртки");
-    wd.findElement(By.id("fTARGET")).click();
-    wd.findElement(By.id("fTARGET")).clear();
-    wd.findElement(By.id("fTARGET")).sendKeys("Учет");
-    wd.findElement(By.id("fANALITIC_VALUE")).click();
-    wd.findElement(By.id("fANALITIC_VALUE")).clear();
-    wd.findElement(By.id("fANALITIC_VALUE")).sendKeys("Контроль");
-    wd.findElement(By.id("fDESCRIPTION")).click();
-    wd.findElement(By.id("fDESCRIPTION")).clear();
-    wd.findElement(By.id("fDESCRIPTION")).sendKeys("Куртки-это удобно");
-    wd.findElement(By.id("fEFFECTIVE_DATE")).click();
-    wd.findElement(By.xpath("//span[contains(text(),\"След\")]"));
-    //wd.findElement(By.linkText("Next")).click();
-    wd.findElement(By.linkText("28")).click();
+    //new Select(wd.findElement(By.id("fSECURITY_ID"))).selectByVisibleText("Открытая Информация"); почему-то этот код не работает!!!
+    wd.findElement(By.id("fCLASS_NAME")).click();//Имя класса
+    wd.findElement(By.id("fCLASS_NAME")).clear();//Имя класса
+    wd.findElement(By.id("fCLASS_NAME")).sendKeys("КурткиАвтотест05");//Имя класса
+    wd.findElement(By.id("fTARGET")).click(); //Цель создания класса
+    wd.findElement(By.id("fTARGET")).clear();//Цель создания класса
+    wd.findElement(By.id("fTARGET")).sendKeys("Учет");//Цель создания класса
+    wd.findElement(By.id("fANALITIC_VALUE")).click();//Аналитическая/эксплуатационная полезность (ценность) класса
+    wd.findElement(By.id("fANALITIC_VALUE")).clear();//Аналитическая/эксплуатационная полезность (ценность) класса
+    wd.findElement(By.id("fANALITIC_VALUE")).sendKeys("Контроль");//Аналитическая/эксплуатационная полезность (ценность) класса
+    wd.findElement(By.id("fDESCRIPTION")).click();//Описание класса
+    wd.findElement(By.id("fDESCRIPTION")).clear();//Описание класса
+    wd.findElement(By.id("fDESCRIPTION")).sendKeys("КурткиАвтотест02-это удобно");//Описание класса
+    wd.findElement(By.id("fEFFECTIVE_DATE")).click();//Дата вступления в силу
+    wd.findElement(By.xpath("//span[contains(text(),\"След\")]"));//Дата вступления в силу
+    wd.findElement(By.linkText("28")).click();//Дата вступления в силу
     wd.findElement(By.id("saveProfile")).click();//Сохранить
     scrollTop();// скроллинг вверх
-    wd.findElement(By.xpath("//a[@id='tab-class-classification']")).click();
+
+    wd.findElement(By.xpath("//a[@id='tab-class-classification']")).click();// вкладка Классификация
     wd.findElement(By.xpath("//input[@id='fBASECLASS']")).click();//клик для открытия дерева
     wd.findElement(By.xpath("//span[@class=\"dynatree-expander\"]")).click();// раскрытие дерева
     wd.findElement(By.xpath("//ul[@class='dynatree-container']/li/ul/li[2]/span/span[1]")).click();//расрытие дерева Товар
     wd.findElement(By.xpath("//div[@id='tCLASSTREE']/ul/li/ul/li[2]/ul/li[2]/span/span[1]")).click();//расрытие дерева Одежда
-    wd.findElement(By.xpath("//a[text()=\"ВЕРХНЯЯ ОДЕЖДА\"]")).click();
-    wd.findElement(By.id("tab-class-properties")).click();
-    new Actions(wd).doubleClick(wd.findElement(By.xpath("//tr[@id='propedit_tr_new']/td[3]"))).build().perform();
-    wd.findElement(By.id("edit_prop_attrname")).click();
-    wd.findElement(By.id("edit_prop_attrname")).clear();
-    wd.findElement(By.id("edit_prop_attrname")).sendKeys("Пуговицы123");
+    wd.findElement(By.xpath("//a[text()=\"ВЕРХНЯЯ ОДЕЖДА\"]")).click();//Выбор базового класса
+
+    wd.findElement(By.id("tab-class-properties")).click();// вкладка Свойства
+    new Actions(wd).doubleClick(wd.findElement(By.xpath("//tr[@id='propedit_tr_new']/td[3]"))).build().perform();//клик
+    // по строке для создания атрибута
+    wd.findElement(By.id("edit_prop_attrname")).click();//Название атрибута
+    wd.findElement(By.id("edit_prop_attrname")).clear();//Название атрибута
+    wd.findElement(By.id("edit_prop_attrname")).sendKeys("Атрибут номер 1");//Название атрибута
     wd.findElement(By.id("saveProfile")).click();//Сохранить
     //выбор типа поля
-    wd.findElement(By.id("ufd-edit_fldtype")).click();
-    wd.findElement(By.xpath("//div[@id='ufd-container']//li[.='Символьный']")).click();
-    if (!wd.findElement(By.xpath("//select[@id='edit_fldtype']//option[6]")).isSelected()) {
-      wd.findElement(By.xpath("//select[@id='edit_fldtype']//option[6]")).click();
-    }
-    //new Select(wd.findElement(By.id("edit_fldtype"))).selectByVisibleText("Вещественный");
-    wd.findElement(By.id("edit_prop_mask")).click();
-    wd.findElement(By.id("edit_prop_mask")).clear();
-    wd.findElement(By.id("edit_prop_mask")).sendKeys("чч");
+    wd.findElement(By.id("ufd-edit_fldtype")).click();//Тип поля
+    wd.findElement(By.xpath("//div[@id='ufd-container']//li[.='Символьный']")).click();//Тип поля
+    if (!wd.findElement(By.xpath("//select[@id='edit_fldtype']//option[6]")).isSelected()) {//Тип поля
+      wd.findElement(By.xpath("//select[@id='edit_fldtype']//option[6]")).click();//Тип поля
+    }//Тип поля
+    wd.findElement(By.id("edit_prop_mask")).click();//Формат/ маска поля
+    wd.findElement(By.id("edit_prop_mask")).clear();//Формат/ маска поля
+    wd.findElement(By.id("edit_prop_mask")).sendKeys("NNSSSS");//Формат/ маска поля
     wd.findElement(By.id("saveProfile")).click();//Сохранить
-    wd.findElement(By.id("edit_prop_limits")).click();
-    wd.findElement(By.id("edit_prop_limits")).clear();
-    wd.findElement(By.id("edit_prop_limits")).sendKeys("1");
+    wd.findElement(By.id("edit_prop_limits")).click();//Ограничения (разрешенные значения)
+    wd.findElement(By.id("edit_prop_limits")).clear();//Ограничения (разрешенные значения)
+    wd.findElement(By.id("edit_prop_limits")).sendKeys("0");//Ограничения (разрешенные значения)
     //wd.findElement(By.id("edit_prop_restriks")).click();
     //wd.findElement(By.id("edit_prop_restriks")).clear();
     //wd.findElement(By.id("edit_prop_restriks")).sendKeys("2");
-    wd.findElement(By.id("edit_prop_defval")).click();
-    wd.findElement(By.id("edit_prop_defval")).clear();
-    wd.findElement(By.id("edit_prop_defval")).sendKeys("3");
-    //new Select(wd.findElement(By.id("edit_prop_seculev"))).selectByVisibleText("Открытая Информация");
-    wd.findElement(By.id("ufd-edit_prop_seculev")).click();
-    wd.findElement(By.xpath("//div[@id='ufd-container']/div[3]/div/div/ul/li[3]")).click();
-    if (!wd.findElement(By.xpath("//select[@id='edit_prop_seculev']//option[3]")).isSelected()) {
-      wd.findElement(By.xpath("//select[@id='edit_prop_seculev']//option[3]")).click();
-    }
+    wd.findElement(By.id("edit_prop_defval")).click();//Значение по-умолчанию
+    wd.findElement(By.id("edit_prop_defval")).clear();//Значение по-умолчанию
+    wd.findElement(By.id("edit_prop_defval")).sendKeys("1");//Значение по-умолчанию
+    wd.findElement(By.id("ufd-edit_prop_seculev")).click();//Уровень секретности
+    wd.findElement(By.xpath("//div[@id='ufd-container']/div[3]/div/div/ul/li[3]")).click();//Уровень секретности
+    if (!wd.findElement(By.xpath("//select[@id='edit_prop_seculev']//option[3]")).isSelected()) {//Уровень секретности
+      wd.findElement(By.xpath("//select[@id='edit_prop_seculev']//option[3]")).click();//Уровень секретности
+    }//Уровень секретности
     wd.findElement(By.id("saveProfile")).click();//Сохранить
-    wd.findElement(By.id("edit_prop_attrdescr")).click();
-    wd.findElement(By.id("edit_prop_attrdescr")).clear();
-    wd.findElement(By.id("edit_prop_attrdescr")).sendKeys("4");
+    wd.findElement(By.id("edit_prop_attrdescr")).click();//Описание атрибута
+    wd.findElement(By.id("edit_prop_attrdescr")).clear();//Описание атрибута
+    wd.findElement(By.id("edit_prop_attrdescr")).sendKeys("Первый атрибут");//Описание атрибута
+    //scrollRight(); //
+    wd.findElement(By.id("edit_prop_attrdate")).click();//Дата вступления в силу
+    wd.findElement(By.linkText("След>")).click();//Дата вступления в силу
+    wd.findElement(By.linkText("След>")).click();//Дата вступления в силу
+    wd.findElement(By.linkText("10")).click();//Дата вступления в силу
+    wd.findElement(By.id("edit_prop_note")).click();//Примечание
+    wd.findElement(By.id("edit_prop_note")).clear();//Примечание
+    wd.findElement(By.id("edit_prop_note")).sendKeys("примечание01");//Примечание
+    wd.findElement(By.id("edit_save")).click();//Сохранить строку
+    wd.findElement(By.id("edit_save")).click();//Сохранить строку
 
-    scrollRight(); //
-    
-    wd.findElement(By.xpath("//table[@id=\"attrs_table\"]//td[13]")).click();
-    wd.findElement(By.id("edit_prop_attrdata")).click();//не находит этот элемент!!!
-    wd.findElement(By.linkText("Next")).click();
-    wd.findElement(By.linkText("5")).click();
-    wd.findElement(By.id("edit_prop_note")).click();
-    wd.findElement(By.id("edit_prop_note")).clear();
-    wd.findElement(By.id("edit_prop_note")).sendKeys("5");
-    wd.findElement(By.id("edit_save")).click();
-    wd.findElement(By.id("tab-class-solutions")).click();
-    wd.findElement(By.id("fEXPLANATORY_NOTE")).click();
-    wd.findElement(By.id("fEXPLANATORY_NOTE")).clear();
-    wd.findElement(By.id("fEXPLANATORY_NOTE")).sendKeys("куртка ага");
-    wd.findElement(By.id("saveProfile")).click();
-    wd.findElement(By.id("sendProfile")).click();
-    wd.findElement(By.linkText("Выход")).click();
+    wd.findElement(By.id("tab-class-solutions")).click();//вкладка Решения
+    wd.findElement(By.id("fEXPLANATORY_NOTE")).click();//пояснительная записка
+    wd.findElement(By.id("fEXPLANATORY_NOTE")).clear();//пояснительная записка
+    wd.findElement(By.id("fEXPLANATORY_NOTE")).sendKeys("автотест пояснительная записка");//пояснительная записка
+    wd.findElement(By.id("saveProfile")).click();//Сохранить профиль
+    wd.findElement(By.id("sendProfile")).click();//Отправить
+
   }
 
-  public void scrollRight() {
-
-    ((JavascriptExecutor) wd).executeScript("window.scrollBy(" + 100 + ","
-            + 0 + ");");
-  }
-
-  private void scrollTop() {
+   private void scrollTop() {
     JavascriptExecutor js = (JavascriptExecutor) wd;
     js.executeScript("window.scrollTo(0, 0);");
   }
